ea23dbe586581717865e54d08ad3dd7466275d0e
Matthieu LAURENT <matthieu.laurent69@proton.me>
2024-02-22 12:22:37 +0100

    Expose StoredFile

--- sqlalchemy_file/__init__.py
+++ sqlalchemy_file/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.6.0"
 
 from .file import File as File  # noqa
+from .stored_file import StoredFile as StoredFile  # noqa
 from .types import FileField as FileField  # noqa
 from .types import ImageField as ImageField  # noqa
